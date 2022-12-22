47f9e9b7124fab89772d57c2bb8eda9c16e4bd91
Kirill <botvenkov2016@yandex.ru>
2022-12-22 22:56:07 +0300

    Задача 2

---
+++ Lesson 2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine() ?? "");
+int b = int.Parse(Console.ReadLine() ?? "");
+int c = int.Parse(Console.ReadLine() ?? "");
+if(a>b && a>c){
+    Console.WriteLine($"Максимальное число {a}");
+}
+else if (b>a && b>c){
+    Console.WriteLine($"Максимальное число {b}");
+}
+else if (c>a && c>b){
+    Console.WriteLine($"Максимальное число {c}");
+}
